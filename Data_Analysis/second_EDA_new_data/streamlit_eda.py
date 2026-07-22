12b1bc46c8ff7e2d883e5cb267b8fb55b8986b64
sage <sagerudder@gmail.com>
2026-07-21 23:51:52 -0400

    try moving data to home directory

--- Data_Analysis/second_EDA_new_data/streamlit_eda.py
+++ Data_Analysis/second_EDA_new_data/streamlit_eda.py
@@ -18,8 +18,14 @@ st.write(
 #replacing old data with new data
 #steam = pd.read_csv('steam.csv')
 #../../Dataset/data/download/steamspy_data.csv
+import os
 
-new_data_path = "steamspy_data.csv"
+
+# folder where this script lives, regardless of cwd
+#BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+#new_data_path = os.path.join(BASE_DIR, "steamspy_data.csv")
+
+new_data_path = "streamspy_data.csv"
 steam = pd.read_csv(new_data_path)
 pd.set_option("display.max_columns", None)
 
